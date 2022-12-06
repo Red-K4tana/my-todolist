0bd874f6e05aed40a2767a8faae41771db06db28
Red-K4tana <ceptor.nill@gmail.com>
2022-12-06 20:43:27 +0300

    appReducer_completed

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,17 +3,18 @@ import {todolistReducer, TodolistsActionType} from "./todolistsReducer";
 import {TasksActionType, tasksReducer} from "./tasksReducer";
 import thunkMiddleware, {ThunkDispatch} from 'redux-thunk';
 import {useDispatch} from "react-redux";
+import {appReducer, AppStateActionType} from "./appReducer";
 
 export const rootReducer = combineReducers({
     todolists: todolistReducer,
     tasks: tasksReducer,
-
+    app: appReducer,
 })
 
 export const rootStore = createStore(rootReducer, applyMiddleware(thunkMiddleware))
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
-export type ActionsType = TodolistsActionType | TasksActionType
+export type ActionsType = TodolistsActionType | TasksActionType | AppStateActionType
 
 //==============================================
 export type TypedDispatch = ThunkDispatch<AppRootStateType, any, ActionsType>
