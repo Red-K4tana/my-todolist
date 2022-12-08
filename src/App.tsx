ed7ee4df5452dcea9c9e7430342296075a05a63b
Red-K4tana <ceptor.nill@gmail.com>
2022-12-09 01:00:04 +0300

    react_router_dom launch_transition_on_Login

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,22 @@ import sl from './App.module.css'
 import {useSelector} from "react-redux";
 import {AppRootStateType} from "./redux/store";
 import { RequestStatusType} from "./redux/appReducer";
+import {Routes, Route, NavLink, Link} from 'react-router-dom';
+import {Login} from "./components/Login/Login";
+import {Error404} from "./components/Error404/Error404";
+import {Button} from "./components/Button/Button";
+
 
 export function App() {
     const appStatusRequest = useSelector<AppRootStateType, RequestStatusType>(state => state.app.status)
+
+
 
     return (
     <div className={sl.app}>
       <header className={sl.appHeader}>
           <h1>TODOLIST</h1>
+
       </header>
         { appStatusRequest === 'loading' &&
         <div className={sl.preloader}>
@@ -22,7 +30,15 @@ export function App() {
         </div>
         }
 
-        <TodolistsList />
+        <>
+            <Routes>
+                <Route path={'/'} element={<TodolistsList />}/>
+                <Route path={'/login'} element={<Login />}/>
+                <Route path={'*'} element={<Error404 />}/>
+            </Routes>
+        </>
+
+
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -5,17 +5,19 @@ import {App} from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from "react-redux";
 import {rootStore} from "./redux/store";
+import {BrowserRouter} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  /*<React.StrictMode>*/
+  <React.StrictMode>
       <Provider store={rootStore}>
-          <App />
+          <BrowserRouter>
+              <App />
+          </BrowserRouter>
       </Provider>
-
- /* </React.StrictMode>*/
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
