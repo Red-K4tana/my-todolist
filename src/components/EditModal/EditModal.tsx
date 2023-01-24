fb06c383132c4580291189484275e2671f7993a6
Red-K4tana <ceptor.nill@gmail.com>
2023-01-24 17:49:32 +0300

    create_EditModal_Component

--- src/components/EditModal/EditModal.tsx
+++ src/components/EditModal/EditModal.tsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+import sl from './EditModal.module.css';
+import {useAppDispatch} from "../../redux/store";
+import {Button} from "../Button/Button";
 
-export const EditModal = () => {
-    return (
-        <div>
 
-        </div>
-    );
-};
+type EditModalPropsType = {
+	viewModeStyle: boolean
+	title: string
+	callbackToDispatchTitle: (newTitle: string) => void
+	callbackToViewMode: (viewMode: boolean) => void
+}
+
+export const EditModal = React.memo((props: EditModalPropsType) => {
+	const dispatch = useAppDispatch()
+
+	const [title, setTitle] = useState(props.title)
+	const changeTitle = (e: ChangeEvent<HTMLInputElement>) => {
+		setTitle(e.currentTarget.value)
+	}
+
+	//====================================================================================================================
+	const saveAndInactivateModal = () => {
+		props.callbackToDispatchTitle(title)
+		props.callbackToViewMode(false)
+	}
+	const pressEnter = (e: KeyboardEvent<HTMLInputElement>) => {
+		if (e.key === 'Enter') {
+			props.callbackToViewMode(false)
+		}
+	}
+	return (
+		<div className={props.viewModeStyle ? sl.activeModal : sl.inactiveModal}>
+			<div className={sl.contentModal}>
+				<input value={title}
+				       onChange={changeTitle}
+				       onBlur={() => props.callbackToViewMode(false)}
+				       onKeyPress={pressEnter}
+				       autoFocus={true}
+				/>
+				<span>counter</span>
+				<Button name={'Save'} callback={saveAndInactivateModal}/>
+			</div>
+		</div>
+	);
+});
--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import {useSelector} from "react-redux";
 import {AppRootStateType, useAppDispatch} from "../../redux/store";
 import {removeTaskAC, removeTaskTC, updateDomainTaskModelType, updateTaskTC} from "../../redux/tasksReducer";
@@ -6,51 +6,61 @@ import sl from '../Todolist/Todolist.module.css'
 import {EditableSpan} from "../EditableSpan/EditableSpan";
 import {Button} from "../Button/Button";
 import {TaskStatuses, TaskType} from "../../API/todolistAPI";
+import {EditModal} from "../EditModal/EditModal";
 
 type TaskPropsType = {
-    todolistID: string
-    taskID: string
+	todolistID: string
+	taskID: string
 }
 
-export const Task = React.memo( (props: TaskPropsType) => {
-    /*console.log('RENDER Task')*/
+export const Task = React.memo((props: TaskPropsType) => {
+	/*console.log('RENDER Task')*/
 
-    const task = useSelector<AppRootStateType, TaskType>(state => state.tasks[props.todolistID]
-        .filter(task => task.id === props.taskID)[0])
-    const dispatch = useAppDispatch()
+	const task = useSelector<AppRootStateType, TaskType>(state => state.tasks[props.todolistID]
+		.filter(task => task.id === props.taskID)[0])
+	const dispatch = useAppDispatch()
 
-    const removeTask = () => {
-        dispatch(removeTaskTC(props.todolistID, props.taskID))
-    }
-    const changeTaskTitle = (newTitle: string) => {
-        const model: updateDomainTaskModelType = {title: newTitle}
-        dispatch(updateTaskTC(props.todolistID, props.taskID, model))
-    }
-    const changeTaskStatus = (e: ChangeEvent<HTMLInputElement>) => {
-        const status = e.currentTarget.checked ? TaskStatuses.Completed : TaskStatuses.New
-        const model: updateDomainTaskModelType = {status}
-        dispatch(updateTaskTC(props.todolistID, props.taskID, model))
-    }
+	const removeTask = () => {
+		dispatch(removeTaskTC(props.todolistID, props.taskID))
+	}
+	const changeTaskTitle = (newTitle: string) => {
+		const model: updateDomainTaskModelType = {title: newTitle}
+		dispatch(updateTaskTC(props.todolistID, props.taskID, model))
+	}
+	const changeTaskStatus = (e: ChangeEvent<HTMLInputElement>) => {
+		const status = e.currentTarget.checked ? TaskStatuses.Completed : TaskStatuses.New
+		const model: updateDomainTaskModelType = {status}
+		dispatch(updateTaskTC(props.todolistID, props.taskID, model))
+	}
+	//====================================================================================================================
+	const [viewMode, setViewMode] = useState<boolean>(false)
 
-
-    return (
-        <div className={task.status === TaskStatuses.Completed ? sl.taskItemCompleted : sl.taskItem}>
-            <Button name={'-'}
-                    callback={removeTask}
-                    style={sl.removeItemButton}
-                    classNameSpanButton={sl.classNameSpanRemoveItem}
-            />
-            <label className={task.status === TaskStatuses.Completed ? sl.checkboxChecked : sl.checkboxUnchecked}>
-              <input className={sl.checkboxTaskStatus}
-                     type={'checkbox'}
-                     checked={task.status === TaskStatuses.Completed}
-                     onChange={changeTaskStatus}
-              />
-            </label>
-            <li className={sl.taskItemLi} >
-                <EditableSpan title={task.title} callback={changeTaskTitle} />
-            </li>
-        </div>
-    );
+	return (
+		<div className={task.status === TaskStatuses.Completed ? sl.taskItemCompleted : sl.taskItem}>
+			<Button name={'-'}
+			        callback={removeTask}
+			        style={sl.removeItemButton}
+			        classNameSpanButton={sl.classNameSpanRemoveItem}
+			/>
+			<label className={task.status === TaskStatuses.Completed ? sl.checkboxChecked : sl.checkboxUnchecked}>
+				<input className={sl.checkboxTaskStatus}
+				       type={'checkbox'}
+				       checked={task.status === TaskStatuses.Completed}
+				       onChange={changeTaskStatus}
+				/>
+			</label>
+			<li className={sl.taskItemLi}>
+				<EditableSpan title={task.title} callback={changeTaskTitle}
+				/>
+				<EditModal viewModeStyle={viewMode} title={task.title} callbackToDispatchTitle={changeTaskTitle}
+				           callbackToViewMode={setViewMode}
+				/>
+			</li>
+			<Button name={' ~ '}
+			        callback={()=>setViewMode(true)}
+			        /*style={}
+			        classNameSpanButton={}*/
+			/>
+		</div>
+	);
 });
-
