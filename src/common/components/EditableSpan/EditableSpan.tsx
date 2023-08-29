9b1ecbdb240ec1b8337e6fab0b3a76e2e15044c4
Red-K4tana <ceptor.nill@gmail.com>
2023-08-29 23:46:56 +0300

    refactored types EditableSpan

--- src/common/components/EditableSpan/EditableSpan.tsx
+++ src/common/components/EditableSpan/EditableSpan.tsx
@@ -1,30 +1,33 @@
-import {ChangeEvent, KeyboardEvent, useState} from 'react';
+import {ChangeEvent, FC, KeyboardEvent, memo, useState} from 'react';
 import sl from 'common/components/EditableSpan/EditableSpan.module.css';
 import {appActions} from 'app/appReducer';
 import {useAppDispatch} from 'common/hooks';
 
-type EditableSpanPropsType = {
+type EditableSpanProps = {
     title: string
     callback: (newTitle:string) => void
 }
 
 
-export const EditableSpan = (props: EditableSpanPropsType) => {
+export const EditableSpan: FC<EditableSpanProps> = memo(({
+                                 title,
+                                 callback,
+                             }) => {
     const [editMode, setEditMode] = useState(false)
     // =================================================================================================================
     const dispatch = useAppDispatch()
-    const [title, setTitle] = useState(props.title)
+    const [valueTitle, setValueTitle] = useState(title)
     const changeTitle = (e: ChangeEvent<HTMLInputElement>) => {
-        setTitle(e.currentTarget.value)
+        setValueTitle(e.currentTarget.value)
     }
     const activateViewMode = () => {
-        if (title.length > 30) {
+        if (valueTitle.length > 30) {
             dispatch(appActions.setAppError({error: 'Title`s length should be less than 27 chars.'}))
-        } else if (title.length <= 0) {
+        } else if (valueTitle.length <= 0) {
             dispatch(appActions.setAppError({error: 'Title should not be empty.'}))
         } else {
             dispatch(appActions.setAppError({error: null}))
-            props.callback(title)
+            callback(valueTitle)
             setEditMode(false)
         }
     }
@@ -38,7 +41,7 @@ export const EditableSpan = (props: EditableSpanPropsType) => {
         editMode
             ?
             <input className={sl.inputEdit}
-                   value={title}
+                   value={valueTitle}
                    onChange={changeTitle}
                    onBlur={activateViewMode}
                    onKeyPress={pressEnter}
@@ -46,7 +49,7 @@ export const EditableSpan = (props: EditableSpanPropsType) => {
             />
             :
             <span onDoubleClick={()=>setEditMode(true)}>
-                {props.title}
+                {title}
             </span>
     );
-};
+});
